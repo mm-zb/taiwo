b3a4cb38cbbca2a176c1350f381bc8d650c27494
zmb24 <zmb24@ic.ac.uk>
2025-08-07 16:18:47 +0100

    Improve security and efficiency of database calls

--- taiwo/friends.py
+++ taiwo/friends.py
@@ -1,5 +1,6 @@
 import sqlite3
 import pickle
+from flask import session
 
 class Graph: 
   #directed graph to form a friends network
@@ -50,3 +51,77 @@ class Graph:
 
         with open('friends_list.pkl', 'wb') as fh:
             pickle.dump(self.adjacency_list, fh)
+
+def register_account(email, user, pwd, cpwd):
+    if not user or not pwd or not cpwd or not email:
+        return 1
+    #checks if all fields are filled out
+    if cpwd != pwd:
+        return 2
+    #checks if password and confirm password have the same value
+    if len(pwd) < 8 or len(pwd) > 16:
+        return 4
+    #checks if password is the correct length 
+
+    with sqlite3.connect('logins.db') as conn:
+        c = conn.cursor()
+
+        c.execute("SELECT * FROM users WHERE username = ?", (user))
+        #executes SQL to copy the whole database
+        data = c.fetchall()
+        #assigns data to variable
+
+        if data:
+            return 3
+        
+        c.execute("INSERT INTO users VALUES (?,?,?,?,?)", (user, pwd, email, 'null', 'null'))
+        #adds username, password and email to database if all checks passed
+
+        conn.commit()
+
+    return 0
+
+def correct_login(username, password):
+    if not username or not password:
+        return 2
+    #checks if all fields are filled out
+
+    with sqlite3.connect("logins.db") as conn:
+        c = conn.cursor()
+
+        c.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
+        data = c.fetchall()
+        #executes SQL to copy entire database to a variable
+
+    if data:
+        return 0
+        #if the username and hashed password matches then log in
+    return 1
+    #no username, password combination found
+
+def add_friend(friend):
+    graph = Graph()
+    user = session.get('account', None)
+    #assign the user's username, and the session friends list to variables
+
+    with sqlite3.connect('logins.db') as conn:
+        c = conn.cursor()
+        c.execute("SELECT usernames FROM users")
+        data = c.fetchall()
+        #executes SQL to copy usernames
+
+    usernames = [d[0] for d in data]
+    #extracts usernames from 1-tuples
+
+    if friend in graph.adjacency_list[user]:
+        return f"{user} is already following {friend}"
+        #if already following the user entered
+    elif friend not in usernames:
+        return f"There is nobody with the username {friend}"
+        #if the username is not taken
+    else:
+        graph.follow(user, friend)
+        graph.save()
+        session['friends'] = graph.adjacency_list
+        return f"Followed {friend} successfully"
+        #if all checks passed, follow the user
