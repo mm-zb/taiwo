ebd13d620d2c5c119ed0ac9d8e13b5ee454bf744
zmb24 <zmb24@ic.ac.uk>
2025-08-07 02:25:58 +0100

    Add function signatures

--- taiwo/spotify_requests.py
+++ taiwo/spotify_requests.py
@@ -2,33 +2,32 @@ from urllib.parse import urlencode
 from urllib.request import urlopen, Request
 import json
 
-import config
-
-client_id = config.SPOTIFY_ID
-client_secret = config.SPOTIFY_SECRET
-
-def request(url, token):
+def _request(url: str, token: str) -> dict:
+    # url: url
+    # token: hash-like token
+    # return: json 
     req = Request(
         url=url, 
-        headers={"Authorization":f"Bearer {str(token)}"}
+        headers={"Authorization":f"Bearer {token}"}
     )
 
     body = urlopen(req).read()
     response = json.loads(body)
     return response
 
-def request_top_songs(token, limit=30):
-    return request(f"https://api.spotify.com/v1/me/top/tracks?limit={limit}", token)
+def request_top_songs(token: str, limit=30) -> dict:
+    return _request(f"https://api.spotify.com/v1/me/top/tracks?limit={limit}", token)
 
-def request_top_artists(token, limit=5):
-    return request(f"https://api.spotify.com/v1/me/top/artists?limit={limit}", token)
+def request_top_artists(token: str, limit=5) -> dict:
+    return _request(f"https://api.spotify.com/v1/me/top/artists?limit={limit}", token)
 
-def clean_top_songs(songs, limit=30):
-
+def clean_top_songs(json_data: dict, limit=30) -> list:
+    # json_data: json dict
+    # return: list of 4-tuple containing song information 
     top_songs = [(
         song['id'],                 #song_id
         song['name'],               #name
         song['artists'][0]['name'], #artist
         song['popularity']          #popularity
-    ) for song in songs['items']]
+    ) for song in json_data['items'][:limit]]
     return top_songs
